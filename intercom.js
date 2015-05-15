633f4fe2156445a2aab749877ee5aaa2026b3d02
Neil Renicker <neilrenicker@gmail.com>
2015-05-15 12:22:02 -0500

    chore: build merged changes

--- intercom.js
+++ intercom.js
@@ -54,13 +54,38 @@ var Intercom = (function() {
 	
 	// --- lib/localstorage.js ---
 	
-	var localStorage = window.localStorage;
-	if (typeof localStorage === 'undefined') {
+	var localStorage = undefined;
+	
+	//Checks if the browser has a working localStorage implementation
+	var hasLocalStorage = (function(localStorage){
+	
+		if (typeof localStorage == 'undefined') {
+			return false;
+		}
+	
+		//Create a test key/val of right now
+		var key = val = "" + Date.now(),
+				hasLS = false;
+	
+		try {
+			localStorage.setItem(key, val);
+			hasLS = (localStorage.getItem(key) == val)
+			localStorage.removeItem(key);
+	
+			return hasLS;
+		} catch (e) {
+			return false;
+		}
+	})(window.localStorage);
+	
+	if (!hasLocalStorage) {
 		localStorage = {
 			getItem    : function() {},
 			setItem    : function() {},
 			removeItem : function() {}
 		};
+	} else {
+		localStorage = window.localStorage
 	}
 	
 	// --- lib/util.js ---
