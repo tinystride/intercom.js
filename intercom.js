b5b6811c7768d26086631f3ef7c2c6865ca1a45e
Neil Renicker <neilrenicker@gmail.com>
2015-05-15 13:04:31 -0500

    feat: more robust localStorage check
    Catches errors for cross-origin security policies.

--- intercom.js
+++ intercom.js
@@ -56,35 +56,17 @@ var Intercom = (function() {
 	
 	var localStorage = undefined;
 	
-	//Checks if the browser has a working localStorage implementation
-	var hasLocalStorage = (function(localStorage){
-	
-		if (typeof localStorage == 'undefined') {
-			return false;
-		}
-	
-		//Create a test key/val of right now
-		var key = val = "" + Date.now(),
-				hasLS = false;
-	
+	// Checks if the browser has a working localStorage implementation
+	var isLocalStorageSupported = function() {
 		try {
-			localStorage.setItem(key, val);
-			hasLS = (localStorage.getItem(key) == val)
-			localStorage.removeItem(key);
-	
-			return hasLS;
-		} catch (e) {
-			return false;
+			window.localStorage['local-storage-test'] = 1;
+			return true
+		} catch (error) {
+			return false
 		}
-	})(window.localStorage);
+	};
 	
-	if (!hasLocalStorage) {
-		localStorage = {
-			getItem    : function() {},
-			setItem    : function() {},
-			removeItem : function() {}
-		};
-	} else {
+	if (isLocalStorageSupported) {
 		localStorage = window.localStorage
 	}
 	
